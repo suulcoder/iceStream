479a37a1c4fb13a1bb6ee90f36be73df2b07cdcf
suulcoder <ef.proyectomundial@gmail.com>
2020-03-06 00:59:49 -0600

    SQL Function Query on Server

--- server.js
+++ server.js
@@ -1,11 +1,36 @@
 const express = require('express');
+const pg = require('pg');
 const app = express();
 
-app.get('/api/customers',(req,res)=>{
-    const customers=[
-        {id:1,firstName:'John'}
-    ];
-    res.json(customers)
-});
+const pool = new pg.Pool({
+    port: 5432,
+    user: 'admin',
+    password: 'admin',
+    database: 'ProyectoBasedeDatos'
+})
+const SQLQuery = (apiRoute,Query) => {
+    app.get(apiRoute,(req,res)=>{
+        pool.connect((err,db,done)=>{
+            if(err){
+                return console.log(err)
+            }
+            db.query(Query,(err,table) => {
+                done();
+                if(err){
+                    return console.log(err)
+                }
+                else{
+                    artists=table.rows
+                    res.json(artists)
+                    return table.rows
+                }
+            })
+        })
+        
+    });
+}
+
+SQLQuery('/api/artists','SELECT * FROM Artist')
+
 const port=5000;
 app.listen(port,()=>console.log(`Server started on port ${port}`))
