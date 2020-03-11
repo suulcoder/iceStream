58f5bbaa500187c308fb912dd1427c59e5c3317f
suulcoder <ef.proyectomundial@gmail.com>
2020-03-10 23:54:34 -0600

    some queries added

--- queries.js
+++ queries.js
@@ -1,49 +1,55 @@
-
-const getAllUsers = () => `SELECT * FROM Customer`;
-
-const getLastCustomerId = () =>
-    (`SELECT max(customerid)\n` +
-        `FROM customer\n`);
-
-const getLastArtistId = () =>
+module.exports = {
+ getAllUsers :`SELECT * FROM Users`,
+ getAllSongs : `SELECT * FROM Track`,
+ getAllGenre : "SELECT * FROM Genre",
+ getAllMediaType : "SELECT * FROM MediaType",
+ getUsersAddPermissions : "SELECT * FROM UserPermission",
+ getCanInactivateSongPermissions : "SELECT * FROM TrackPermission WHERE canInactivate=True GROUP BY UserId",
+ getCanUpdateSongPermissions : "SELECT * FROM TrackPermission WHERE canUpdate=True GROUP BY UserId",
+ getCanDeleteSongPermissions : "SELECT * FROM TrackPermission WHERE canDelete=True GROUP BY UserId",
+ getCanUpdateArtistPermissions : "SELECT * FROM ArtistPermission WHERE canUpdate=True GROUP BY UserId",
+ getCanDeleteArtistPermissions : "SELECT * FROM ArtistPermission WHERE canDelete=True GROUP BY UserId",
+ getCanUpdateAlbumPermissions : "SELECT * FROM AlbumPermission WHERE canUpdate=True GROUP BY UserId",
+ getCanDeleteAlbumPermissions : "SELECT * FROM AlbumPermission WHERE canDelete=True GROUP BY UserId",
+ getLastUserId :
+    (`SELECT max(Userid)\n` +
+        `FROM Users\n`),
+ getLastArtistId :
     ("SELECT max(artistid)\n" +
-        "from artist");
-
-const getAllPlaylistInfo = () =>
+        "from artist"),
+ getAllPlaylistInfo :
     (`SELECT p1.name\n` +
         `from playlist p1\n` +
-        `order by p1.name`);
-
-const getAllSongsInPlaylist = (playlistName) => `SELECT p1.name, t1.name\n` +
+        `order by p1.name`),
+ getAllSongsInPlaylist : (`SELECT p1.name, t1.name\n` +
     `from playlist p1 join playlisttrack pt1 on p1.playlistid = pt1.playlistid join track t1 on t1.trackid = pt1.trackid\n` +
-    `where p1.name = '${playlistName}'\n` +
-    `order by p1.name, t1.name`;
-
-const getPermitFromUser = (user) => `SELECT p1.permit\n` +
-    `from customer c1 join permissions p1 on c1.customerid = p1.customerid\n` +
-    `where customer = "${user}"`;
-
-const getMostCommonGenres = () => 'SELECT g1.name, count(g1.name)\n' +
+    `where p1.playlistid = $1\n` +
+    `order by p1.name, t1.name`),
+ getMostCommonGenres : ('SELECT g1.name, count(g1.name)\n' +
     'from track t1 join genre g1 on t1.genreid = g1.genreid\n' +
     'group by g1.name\n' +
-    'order by count(g1.name) desc';
-
-const getMostCommonArtist = () => 'SELECT ar1.name, count (ar1.name)\n' +
+    'order by count(g1.name) desc'),
+ getMostCommonArtist : ('SELECT ar1.name, count (ar1.name)\n' +
     'from album al1 join artist ar1 on al1.artistid = ar1.artistid\n' +
     'group by ar1.name\n' +
-    'order by count(ar1.name) desc';
-
-const getLongestSongsWithArtists = () => 'SELECT t1.name, t1.milliseconds, a1.artistid, a1.name\n' +
+    'order by count(ar1.name) desc'),
+ getLongestSongsWithArtists : ('SELECT t1.name, t1.milliseconds, a1.artistid, a1.name\n' +
     'from track t1 join artist a1 on t1.composer = a1.name\n' +
-    'order by t1.milliseconds desc';
-
-const getDurationAverageByGenre = () => 'SELECT g1.name, avg(t1.milliseconds)\n' +
+    'order by t1.milliseconds desc'),
+ getDurationAverageByGenre : ('SELECT g1.name, avg(t1.milliseconds)\n' +
     'from track t1 join genre g1 on t1.genreid = g1.genreid\n' +
     'group by g1.name\n' +
-    'order by avg(t1.milliseconds) desc';
-
-const getMostColaborativeArtists = () => 'SELECT ar1.name, count(ar1.name)\n' +
+    'order by avg(t1.milliseconds) desc'),
+ getMostColaborativeArtists : ('SELECT ar1.name, count(ar1.name)\n' +
     'from album al1 join artist ar1 on al1.artistid = ar1.artistid join track t1 on t1.composer = ar1.name\n' +
     'group by ar1.name\n' +
-    'order by count(ar1.name) desc';
-
+    'order by count(ar1.name) desc'),
+ addArtist : "INSERT INTO Artist (ArtistId, Name) VALUES ($1,$2);",
+ addAlbum : "INSERT INTO Album (AlbumId, Title, ArtistId) VALUES ($1,$2,$3);",
+ addTrack : "INSERT INTO Track (TrackId, Name, AlbumId, MediaTypeId, GenreId, Composer, Milliseconds, Bytes, UnitPrice) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9);",
+ addUser : "INSERT INTO Users (UserId,Username,email,password,role) VALUES ($1,$2,$3,$4,$5);",
+ UpdateArtist : "UPDATE Artist SET Name=$2 WHERE Userid=$1;",
+ UpdateUser : "UPDATE Users SET Username=$2, email=$3, password=$4,role=$5 WHERE UserId=$1;",
+ UpdateAlbum : "UPDATE Album SET Title=$2, ArtistId=$3 where AlbumID=$1",
+ UpdateTrack : "UPDATE Track SET Name=$2, AlbumId=$3, MediaTypeId=$4, GenreId=$5, Composer=$6, Millisecons=$7, Bytes=$8, UnitPrice=$9 WHERE TrackId=$1",
+}
--- server.js
+++ server.js
@@ -1,11 +1,7 @@
+const query = require('./queries')
 const express = require('express');
 const pg = require('pg');
 const app = express();
-
-app.use(function(request,response,next){
-    response.header("Access-Control-Allow-Origin","*");
-    response.header("Access-Control-Allos-Headers","Origin, X-Requested-With,Content-Type")
-})
 
 const pool = new pg.Pool({
     port: 5432,
@@ -16,25 +12,24 @@ const pool = new pg.Pool({
 const SQLQuery = (apiRoute,Query,method='get') => {
     switch (method) {
         case 'get':
-            app.get(apiRoute,(req,res)=>{
+            app.get(apiRoute,(request,res)=>{
                 pool.connect((err,db,done)=>{
                     if(err){
-                        return response.status(400).send(err)
+                        return res.status(400).send(err)
                     }
                     db.query(Query,(err,table) => {
                         done();
                         if(err){
-                            return response.status(400).send(err)
+                            return res.status(400).send(err)
                         }
-                        artists=table.rows
-                        res.json(artists)
+                        console.log(table.rows)
                         return table.rows
                     })
                 })
             });
         case 'post':
-            const values = Object.values(request.body)
             app.post(apiRoute,function(request,response){
+                const values = Object.values(request.body)
                 pool.connect((err,db,done)=>{
                     if(err){
                         return response.status(400).send(err)
@@ -54,11 +49,16 @@ const SQLQuery = (apiRoute,Query,method='get') => {
                 })
             });
     }
+    return null
 }
-SQLQuery('/api/user','SELECT * FROM User')
-SQLQuery('/api/artists','SELECT * FROM Artist')
-SQLQuery('/api/newArtist',"INSERT INTO Artist (ArtistId, Name) VALUES ($1,$2);",'post')
-SQLQuery('api/newAlbum',"INSERT INTO Album (AlbumId, Title, ArtistId) VALUES ($1,$2,$3);",'post')
-SQLQuery('/api/newTrack',"INSERT INTO Track (TrackId, Name, AlbumId, MediaTypeId, GenreId, Composer, Milliseconds, Bytes, UnitPrice) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9);",'post')
+SQLQuery('/api/user',query.getAllUsers)
+SQLQuery('/api/genre',query.getAllGenre)
+SQLQuery('/api/permission/add',query.getUsersAddPermissions)
+SQLQuery('/api/permission/canIncativateSong',query.getCanInactivateSongPermissions)
+SQLQuery('/api/report/mostColaborative',query.getMostColaborativeArtists)
+SQLQuery('/api/report',query.getMostColaborativeArtists)
+SQLQuery('/api/newArtist',query.addArtist,'post')
+SQLQuery('api/newAlbum',query.addAlbum,'post')
+SQLQuery('/api/newTrack',query.addTrack,'post')
 const port=8080;
 app.listen(port,()=>console.log(`Server started on port ${port}`))
